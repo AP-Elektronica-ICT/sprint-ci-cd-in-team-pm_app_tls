24fa83dfc125d3f35ae80ac014e40a9fca68a50d
Tijl Ivens <tijl.ivens@hotmail.be>
2019-01-09 20:18:27 +0100

    Activitie1

--- app/src/main/java/studio/stijl/tsl_app/MainActivity.java
+++ app/src/main/java/studio/stijl/tsl_app/MainActivity.java
@@ -1,8 +1,10 @@
 package studio.stijl.tsl_app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.crashlytics.android.Crashlytics;
 import io.fabric.sdk.android.Fabric;
@@ -14,6 +16,39 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
+        final Button mActivitie1 = findViewById(R.id.activitie1Button);
+        mActivitie1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),Activitie1.class);
+                startActivity(i);
+            }
+        });
+        final Button mActivitie2 = findViewById(R.id.activitie2Button);
+        mActivitie2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent i = new Intent(getApplicationContext(),Activitie2.class);
+                //startActivity(i);
+            }
+        });
+        final Button mActivitie3 = findViewById(R.id.activitie3Button);
+        mActivitie3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent i = new Intent(getApplicationContext(),Activitie3.class);
+                //startActivity(i);
+            }
+        });
+        final Button mButton = findViewById(R.id.button4);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent i = new Intent(getApplicationContext(),Activitie2.class);
+                //startActivity(i);
+                throw new RuntimeException("This is a crash2");
+            }
+        });
     }
 
     public void forceCrash(View view) {
